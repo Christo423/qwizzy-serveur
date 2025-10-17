afd98061c0de95429243c361556e20ca1c2e327a
Christo423 <141580952+Christo423@users.noreply.github.com>
2025-10-17 22:00:07 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -40,10 +40,7 @@ def clee():
 #envoie les questions 
 @app.route('/recupQuestions', methods=['GET'])
 def recupQuestions():
-    if questions_liste :
-        return jsonify({"questions": questions_liste})
-    else :
-        return jsonify({"questions": "aucune donnes"})
+    return jsonify({"questions": questions_liste})
 # ----------- Démarrage serveur -----------
 
 if __name__ == '__main__':
